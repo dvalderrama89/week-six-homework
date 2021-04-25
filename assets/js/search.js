b9350808d325b0a5f990bdafbc68f056e5604f7e
David Valderrama <dvalderrama08@gmail.com>
2021-04-24 22:27:25 -0700

    Fixed the day display logic and how it renders to the cards

--- assets/js/search.js
+++ assets/js/search.js
@@ -28,57 +28,82 @@ function getWeatherData(term) {
 }
 
 function renderForecast(data) {
+    console.log(data);
     let cityName = data.city.name;
     let dayList = data.list;
 
     // Clear out any old elements on a new search
-    $("#cityInfoContainer").empty();
+    // $("#cityInfoContainer").empty();
     $("#fiveDayForecast").empty();
 
+    // There's either the next 6 days of content including today or starting from tomorrow
+    // depending on how late in the day you call the API    
+    let daysReturnedByAPI = [];
+    if (moment().format("l") === dayList[0].dt_txt) {
+        for (let i = 0; i < 6; i++) {
+            daysReturnedByAPI.push(moment().add(i, "days").format("l"));
+        }
+    } else {
+        for (let i = 1; i < 7; i++) {
+            daysReturnedByAPI.push(moment().add(i, "days").format("l"));
+        }
+    }
+
+    console.log(daysReturnedByAPI);
+
+    let dayNotRendered = false;
     // Display the information that's at 12:00pm each day
-    for (let index = 0; index < dayList.length; index++) {
-        if (dayList[index].dt_txt.includes("12:00:00")) {
-            renderForecastCard(cityName, dayList[index]);
+    
+    for (let day = 0; day < daysReturnedByAPI.length; day++) {
+        for (let index = 0; index < dayList.length; index++) {
+            if (moment(dayList[index].dt_txt).format("l").includes(daysReturnedByAPI[day])) {
+                renderForecastCard(cityName, dayList[index], index);
+                break;
+            }
         }
     }
 }
 
 // Contains all the data of a particular day at 12:00 pm
-function renderForecastCard(cityName, dayObj) {    
+function renderForecastCard(cityName, dayObj, index) {    
     let date = dayObj.dt_txt;
     let temp = dayObj.main.temp;
     let humidity = dayObj.main.humidity;
     let windSpeed = dayObj.wind.speed;
 
-    let dayTitle = moment(date).format("l");
-    let tempText = $("<p>").addClass("card-text").text(`Temp: ${temp} ℉`);
-    let humidityText = $("<p>").addClass("card-text").text(`Humidity: ${humidity}%`);
 
-    let newCardColumn = $("<div>").addClass("col");
-    let newCardElem = $("<div>").addClass("card");
-    let newCardBody = $("<div>").addClass("card-body");
-
-    newCardBody.append(dayTitle);
-    newCardBody.append(tempText);
-    newCardBody.append(humidityText);
-
-    newCardElem.append(newCardBody);
-    newCardColumn.append(newCardElem);
-
-    $("#fiveDayForecast").append(newCardColumn);
-
-    // Add info to main card (Note: UV was deprecated April 1st 2021)
-    if (dayTitle === moment().format("l")) {
+    // Add info to main card at the top if it's the first day (Note: UV was deprecated April 1st 2021)
+    if (index === 0) {
         let cardBody = $("<div>").addClass("card-body");
         let cardTitle = $("<h5>").addClass("card-title").text(`${cityName} (${moment(date).format("l")})`);
-        let cardTemp = $("<p>").addClass("card-text").text(`Temperature: ${temp} ℉`);
-        let cardHumidity = $("<p>").addClass("card-text").text(`Humidity: ${humidity}%`)
+        let cardTemp = $("<p>").addClass("card-text").text(`Temperature: ${Math.floor(temp)} ℉`);
+        let cardHumidity = $("<p>").addClass("card-text").text(`Humidity: ${humidity}%`);
         let cardWindSpeed = $("<p>").addClass("card-text").text(`Wind speed: ${windSpeed} MPH`);
 
         cardBody.append(cardTitle);
         cardBody.append(cardTemp);
         cardBody.append(cardHumidity);
         cardBody.append(cardWindSpeed);
+        
+        $("#cityInfoContainer").empty();
         $("#cityInfoContainer").append(cardBody);
+    } else {
+            
+        let dayTitle = $("<h5>").addClass("card-title").text(moment(date).format("l"));
+        let tempText = $("<p>").addClass("card-text").text(`Temp: ${Math.floor(temp)} ℉`);
+        let humidityText = $("<p>").addClass("card-text").text(`Humidity: ${humidity}%`);
+
+        let newCardColumn = $("<div>").addClass("col");
+        let newCardElem = $("<div>").addClass("card");
+        let newCardBody = $("<div>").addClass("card-body bg-primary text-white");
+
+        newCardBody.append(dayTitle);
+        newCardBody.append(tempText);
+        newCardBody.append(humidityText);
+
+        newCardElem.append(newCardBody);
+        newCardColumn.append(newCardElem);
+
+        $("#fiveDayForecast").append(newCardColumn);
     }
 }
